8cf41e0f0d98e521d3def691d32a4fe2d4f59cda
aenjeaer <anjarwati.091001@gmail.com>
2023-03-14 13:33:48 +0900

    menambahkan icon website

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -4,12 +4,12 @@ styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 // hide style switcher on scroll 
-window.addEventListener("scroll", () => {
-    if(document.querySelector(".style-switcher").classList.contains("open"))
-    {
-        document.querySelector(".style-switcher").classList.remove("open");
-    }
-})
+// window.addEventListener("scroll", () => {
+//     if(document.querySelector(".style-switcher").classList.contains("open"))
+//     {
+//         document.querySelector(".style-switcher").classList.remove("open");
+//     }
+// });
 /* =================== Theme Color =================== */
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color)
