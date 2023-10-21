6217b0e13d129b4218ba02ccd453b5e6ad5c42e9
aenjeaer <124568898+aenjeaer@users.noreply.github.com>
2023-10-21 11:53:52 +0900

    perbaiki konfigurasi matkul 2

--- portfolio/materikuliah/login_system_admin_page.php
+++ portfolio/materikuliah/login_system_admin_page.php
@@ -10,32 +10,3 @@ if(!isset($_SESSION['admin_name'])){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>admin page</title>
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/login_system_style.css">
-
-</head>
-<body>
-   
-<div class="container">
-
-   <div class="content">
-      <h3>hi, <span>admin</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
-      <p>this is an admin page</p>
-      <a href="login_system_login_form.php" class="btn">login</a>
-      <a href="login_system_register_form.php" class="btn">register</a>
-      <a href="login_system_logout.php" class="btn">logout</a>
-   </div>
-
-</div>
-
-</body>
-</html>
--- portfolio/materikuliah/login_system_user_page.php
+++ portfolio/materikuliah/login_system_user_page.php
@@ -10,32 +10,3 @@ if(!isset($_SESSION['user_name'])){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>user page</title>
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/login_system_style.css">
-
-</head>
-<body>
-   
-<div class="container">
-
-   <div class="content">
-      <h3>hi, <span>user</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
-      <p>this is an user page</p>
-      <a href="login_system_login_form.php" class="btn">login</a>
-      <a href="login_system_register_form.php" class="btn">register</a>
-      <a href="login_system_logout.php" class="btn">logout</a>
-   </div>
-
-</div>
-
-</body>
-</html>
