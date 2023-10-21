353333a5bc3795910a39bb07a7ae6594fa72189e
aenjeaer <124568898+aenjeaer@users.noreply.github.com>
2023-10-21 11:39:03 +0900

    perbaiki konfigurasi file markul

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ quiz2.addEventListener("click", () => {
 // kumpulan matkul
 const kumpulanmatkul = document.getElementById('materikuliah');
 kumpulanmatkul.addEventListener("click", () => {
-    window.open("/portfolio/materikuliah/filematkul.html", "_blank");
+    window.open("/portfolio/materikuliah/login_system_login_form.html", "_blank");
 })
 // login form
 const loginform = document.getElementById('loginform');
